21029fb85a18013d91814b875b93f93dfb10728e
Benjamin <benjamin@stenberg-jakobsen.dk>
2019-11-28 14:15:46 +0100

    leaderboard and stats

--- app/src/Screens/DashboardScreen/LeaderBoardPage/index.js
+++ app/src/Screens/DashboardScreen/LeaderBoardPage/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import './index.css';
 
-function __Component__(props){
+const players = ["Bob", "Eric", "David", "William", "Benjamin", "Jesus", "Stone", "Poul", "Victor", "Daniel"]
+function LeaderBoardPage(props){
   return (
     <>
+    <table style={{
+      width : "40%",
+      border : "1px solid black",
+      margin : "auto",
+      textAlign : "left",
+      borderSpacing : "2vmax",
+      marginTop : "10vh"
+
+    }}>
+      <caption style={{
+        fontWeight : "700",
+        fontSize : "1.5rem",
+        marginBottom : "2vh"
+      }}>Global leaderboard</caption>
+      <tr>
+        <th>Rank</th>
+        <th>Username</th>
+      </tr>
+      {
+        players.map((player, idx) => {
+          return (
+            <tr>
+              <td>{idx+1}</td>
+              <td>{player}</td>
+            </tr>
+          );
+        })
+      }
+    </table>
+
     </>
   );
 }
 
-export default __Component__;
+export default LeaderBoardPage;
--- app/src/Screens/DashboardScreen/StatsPage/index.js
+++ app/src/Screens/DashboardScreen/StatsPage/index.js
@@ -1,11 +1,108 @@
 import React from 'react';
 import './index.css';
+import {
+  useHistory
+} from 'react-router-dom';
 
-function __Component__(props){
+const pages = {
+  CHALLENGES : "challenges",
+  STATS : "stats",
+  LEADERBOARD : "leaderboard",
+  EDITPROFILE : "edit-profile",
+}
+
+function StatsPage(props){
+  const history = useHistory();
   return (
-    <>
-    </>
+    <div className = {"StatsPage-divBox"} style={{
+      height : "75vh",
+      width : "50%",
+      marginLeft : "7vw",
+      marginTop : "6vh",
+      borderRadius : "15px"
+    }}>
+      <div style={{
+        height : "10vh",
+        margin : "auto"}}>
+      </div>
+      <div onClick={() =>{
+        history.push(`/dashboard/${pages.CHALLENGES}`)
+      }} style={{
+        cursor : "pointer",
+        width : "70%", 
+        height : "10vh",
+        margin : "auto",
+        fontWeight : "700",
+        fontSize : "1.5rem"
+      }}>
+        <p style={{
+          margin : "0"
+        }} className={"StatsPage-text"}>Challenges completed: X/X</p>
+      </div>
+      <div style={{
+        cursor : "pointer",
+        width : "70%", 
+        height : "10vh",
+        margin : "auto",
+        fontWeight : "700",
+        fontSize : "1.5rem"
+      }}>
+        <p style={{
+          margin : "0"
+        }} className={"StatsPage-text"}>Points: Y</p>
+      </div>
+      <div onClick={() =>{
+        history.push(`/dashboard/${pages.LEADERBOARD}`)
+      }} style={{
+        cursor : "pointer",
+        width : "70%", 
+        height : "10vh",
+        margin : "auto",
+        fontWeight : "700",
+        fontSize : "1.5rem"
+      }}>
+        <p style={{
+          margin : "0"
+        }}className={"StatsPage-text"}>Rating: Z</p>
+      </div>
+      <div onClick={() =>{
+        history.push(`/dashboard/${pages.LEADERBOARD}`)
+      }} style={{
+        cursor : "pointer",
+        width : "70%", 
+        height : "10vh",
+        margin : "auto",
+        fontWeight : "700",
+        fontSize : "1.5rem"
+      }}>
+        <p style={{
+          margin : "0"
+        }} className={"StatsPage-text"}>Leaderboard Rank: 1</p>
+      </div>
+      <div style={{
+        width : "70%", 
+        height : "10vh",
+        margin : "auto",
+        fontWeight : "700",
+        fontSize : "1.5rem"
+      }}>
+        Rank: Master
+      </div>
+      <div style={{
+        width : "70%", 
+        height : "10vh",
+        margin : "auto",
+        fontWeight : "700",
+        fontSize : "1.5rem"
+      }}>
+        Favourite champion: Brand
+      </div>
+      
+
+    </div>
+    
+    
   );
 }
 
-export default __Component__;
+export default StatsPage;
